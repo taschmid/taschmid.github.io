5f668e90fab313d7f7804cd32ba8dae2573628cf
Taylor Schmidt <taschmid@mymail.mines.edu>
2014-05-23 18:18:26 -0600

    hello

--- cgi-bin/emailSaver.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
- 
-#import cgi, cgitb 
- 
-form = cgi.FieldStorage() 
- 
-firstName = form.getvalue('firstName') 
-lastName = form.getvalue('lastName') 
-email = form.getvalue('email')
